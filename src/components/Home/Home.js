4ab5f61609a769c7fa2a28e60215dbcf2cd836b3
Riday1 <rrriday100@gmail.com>
2022-12-07 18:38:40 +0600

    data is loaded from public folder using useEffect

---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Cards from '../Cards/Cards';
+import Navbar from '../Navbar/Navbar';
+import './Home.css'
+const Home = () => {
+
+
+    const [exercises, setExercises] = useState([]);//fetch json data 
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setExercises(data))
+    }, [])
+
+    return (
+        <div className='main-container'>
+            <div className='left-part'>
+                <div className="left-container">
+                    <Navbar></Navbar>
+                    
+                </div>
+            </div>
+            <div className="right-container">
+                <h1>hello</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
