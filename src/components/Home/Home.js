924db4ddcc3d5337a96ff372e43d31875b789099
Riday1 <rrriday100@gmail.com>
2022-12-07 22:33:05 +0600

    user information section is created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Cards from '../Cards/Cards';
 import Navbar from '../Navbar/Navbar';
 import './Home.css'
+import userImage from '../../user-image/user-1.png'
 const Home = () => {
 
 
@@ -21,7 +22,23 @@ const Home = () => {
                 </div>
             </div>
             <div className="right-container">
-                <h1>hello</h1>
+                <div className='user-info'>
+                    <div className="userProfile">
+                        <img className='user-image' src={userImage} alt="" />
+                        <div className='user-details'>
+                            <h2 className='user-name'>Arnob Kormokar Shishir </h2>
+                            <p className='user-address'><svg className='icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                            </svg>
+                                Dhaka , Bangladesh</p>
+                        </div>
+                    </div>
+                    <div className="healthDetails"></div>
+                    <div className="breakTime"></div>
+                    <div className="userExerciseDetials"></div>
+
+                </div>
             </div>
         </div>
     );
