99fe64356fcb92effee9e55af87ce750984e0b82
Riday1 <rrriday100@gmail.com>
2022-12-07 18:48:31 +0600

    cards component is created and rendered in home js file

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Cards.css'
+import Card from '../Card/Card'
+const Carts = ({ exercises }) => {
+    return (
+        <div>
+            <h3 className='select-title'>Select today's exercise</h3>
+            <div className='cards-container'>
+                {
+                    
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Carts;
